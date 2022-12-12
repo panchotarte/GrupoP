022c746b095b05b25fd207daf051b9dbcb5bbedb
panchotarte <110926098+panchotarte@users.noreply.github.com>
2022-12-12 20:53:30 -0300

    Ultima version

--- Probando 2.R
+++ Probando 2.R
@@ -1,3 +1,7 @@
+####
+rm(list=ls())
+
+
 #Instalamos algunos paquetes
 install.packages("readr")
 install.packages("lmtest")
@@ -9,10 +13,19 @@ install.packages("assertthat")
 install.packages("car")
 install.packages('tseries')
 install.packages('sandwich')
+install.packages('ivreg')
+install.packages("AER")
+install.packages('data.table')
+install.packages('psych')
+install.packages('jtools')
+install.packages('skedastic')
+
+
 
 #los activamos
 library("readr")
 library("haven")
+library("zoo")
 library("lmtest")
 library("dplyr")
 library("datos")
@@ -22,6 +35,15 @@ library("car")
 library("tseries")
 library("ggplot2")
 library("sandwich")
+library("ivreg")
+library("AER")
+library("data.table")
+library("psych")
+library("jtools")
+library("skedastic")
+
+
+
 
 ####
 ###
@@ -58,14 +80,16 @@ base_select <- select(base,
                       filtroedad_a1,
                       
                       #Otras
-                      ahv98,
-                      
+                      Hvbañosi, # vivienda con baño
+                      Hvcocina, #lugar para cocinar en la casa
+                      acg1, #estado civil
+                    
                 
                       ) 
 #####
 ####
 # NOMBRAR VARIABLES
-c = c("pcard","refresco" ,"carne", "huevo", "snacks", "cafe", "sal", "massal","comirapida", "hiper", "diabet", "obesi", "insufrenal", "hombre", "edad", "actfisica", "filtroedad", "frecfuma" )
+c = c("pcard","refresco" ,"carne", "huevo", "snacks", "cafe", "sal", "massal","comirapida", "hiper", "diabet", "obesi", "insufrenal", "hombre", "edad", "actfisica", "filtroedad", "baño", "cocina", "estadocivil" )
 names(base_select) = c
 #####
 ####
@@ -91,6 +115,11 @@ base_select$snacks <-  recode(base_select$snacks, "1=7; 2=3; 3=2; 4=0.7; 5=0")
 base_select$refresco <-  recode(base_select$refresco, "1=7; 2=3; 3=2; 4=0.7; 5=0")
 base_select$actfisica <-  recode(base_select$actfisica, "1=3; 2=2; 3=1")
 base_select$comirapida <-  recode(base_select$comirapida, "1=7; 2=3; 3=2; 4=0.7; 5=0")
+base_select$baño <-  recode(base_select$baño, "1=2; 2=1; 3=0")
+base_select$cocina <-  recode(base_select$cocina, "1=2; 2=1; 3=0")
+base_select$estadocivil <-  recode(base_select$estadocivil, "1=2; 2=1; 3=0")
+
+
 
 base_select$massal <-  recode(base_select$massal, "1=0; 2=1; 3=2")
 
@@ -126,6 +155,7 @@ edad <-  (base_select$edad)
 hombre <-  (base_select$hombre)
 huevo <-  (base_select$huevo)
 actfisica <- (base_select$actfisica)
+cocina*sal <- (base_select$cocina)
 
 table(base_select$sal)
 table(base_select$hiper)
@@ -133,77 +163,86 @@ table(base_select$hiper)
 ######
 #####
 ####
-base_select <- base_select %>%  mutate(edad2, carne*sal, snacks*refresco, huevo*obesi, obesi*hiper, sal*snacks, actfisica*obesi)
+base_select <- base_select %>%  mutate(edad2, carne*sal, snacks*refresco, huevo*obesi, obesi*hiper, sal*snacks, actfisica*obesi, cocina*snacks)
 #####
 ####
 ####
 ##
 # Tiramos el primer MPL de encontrar un problema cardiaco 
-PrimerModelo <- lm(pcard ~  huevo + cafe + refresco +  snacks + hiper + obesi + actfisica +  hombre  + edad + edad2, data = base_select)
+ModeloBasico <- lm(pcard ~  huevo + cafe + refresco +  snacks + hiper + obesi + actfisica +  hombre  + edad + edad2, data = base_select)
+summary(ModeloBasico)
+#####
+######
+#####
+PrimerModelo <-  lm(pcard ~ huevo + cafe + refresco +  snacks + hiper + obesi + actfisica +  hombre  + edad + edad2 + sal+ snacks*sal, data = base_select)
 summary(PrimerModelo)
-#####
-######
-#####
-Modelo1 <-  lm(pcard ~ huevo + cafe + refresco +  snacks + hiper + obesi + actfisica +  hombre  + edad + edad2 + sal+ snacks*sal, data = base_select)
-summary(Modelo1)
 ####
-# Tiramos el segundo MPL de encontrar un problema cardiaco 
-Modelo2 <- lm(pcard ~ refresco + carne + sal + massal + huevo + snacks + hombre + edad, data = base_select)
-summary(Modelo2)
+# Tiramos el modelo con su forma reducida tomando como variable dependiente snaks y instrumento cocina.
+forma.reducida <-  lm(snacks ~ huevo + cafe + refresco + hiper + obesi + actfisica +  hombre  + edad + edad2 + sal + cocina , data = base_select)
+summary(forma.reducida)
 #####
 ######
 #####
 ####
-
-
+### Variables instrumentales, iverg. 
+Modelo1instru<- ivreg(pcard ~ huevo + cafe + refresco+ snacks +  hiper + obesi + actfisica+ hombre +edad + edad2+ sal +snacks*sal |
+                        huevo + cafe + refresco + hiper + obesi + actfisica + hombre + edad + edad2 + sal +  cocina + cocina*sal, data = base_select)
+summary(Modelo1instru)
+########
+####
+####
+#Creamos compenetens necesarios para tirar el modelo de minimos cuadrados ponderados
+base_select$cuad.res.m1 <- PrimerModelo$residuals^2      #Agregamos la variable cuadrados de los residuos
+base_select$var <- predict.lm(PrimerModelo)*(1-predict.lm(PrimerModelo)) #La varianza para cada una de las observaciones ( de los errores condicionales)
+pond <- 1/sqrt(base_select$var)
+###Modelo de minimos cuadrados ponderados
+PrimerModeloMCP <-  lm(pcard ~ huevo + cafe + refresco +  snacks + hiper + obesi + actfisica +  hombre  + edad + edad2 + sal+ snacks*sal , data = base_select, weights =pond )
+summary(PrimerModeloMCP)
 
 ########################
 ####################
 #Prueba reset
-resettest(Modelo1, 
+resettest(PrimerModeloMCP, 
           power = 2:3, 
           type = "fitted") 
-#######################3
-
-
-
-
-
-forma.reducida= lm(hiper ~ huevo + cafe + refresco +  snacks + hiper + obesi + actfisica +  hombre  + edad + edad2  )
-
-
-
-
+#######################
 ######################################################################################################################################################################################
 ############################################################################################################################################################
 ########################################################################################################
 ########  VIDEO MPL MANUEL
-## Grafico valores ajustados contra valores observados
-base_select$prediccion <- predict.lm(Modelo1)
+#Agregamos los valores de prediccion a la tabla
+base_select$prediccion <- predict.lm(PrimerModelo)
+base_select$pred <- predict.lm(PrimerModeloMCP)
+
+
+
+####
+##
+#
 data_mod <- data.frame(Predicted = predict(Modelo1), Observed = base_select$pcard)
 ggplot(data_mod, aes(x = Predicted,  y = Observed))+
   geom_point(alpha=0.1)+
   geom_smooth(method="lm", se=FALSE)+
-  scale_y_continuous(breaks = c(1,2), labels=c("1","2"))+
+  scale_y_continuous(breaks = c(0,1), labels=c("0","1"))+
   labs(x="Predicción del modelo", y="Valor observado o probabilidad de 1 predicha")+
   theme_bw()
 abline(h = 1, lty = 2, col = "darkred")
 abline(h = 0, lty = 2, col = "darkred")
-text(2.5, 1.6, cex = 0.8, "pcard")
-text(2.5, -0.1, cex= 0.8, "no pcard")
+text(2.5, 0.9, cex = 0.8, "Cesárea")
+text(2.5, -0.1, cex= 0.8, "Vaginal")
 
 ## Histograma de residuos
 hist(Modelo1$residuals, main = "Histograma de residuos del Modelo 1", xlab = "Residuos del Modelo 1")
 
-base_select <- mutate(pcard_hat=prediccion)
-
+#Calculamos la proporcion de problemas cardiacos para usar en la bondad de ajuste.
+table(base_select$pcard)
+131/1359
 ## Bondad de ajuste: Tabla de predicciones correctas e incorrectas
-base_select$pcard_hat <- ifelse(base_select$prediccion > 0.5, 1, 0)
+base_select$pcard_hat <- ifelse(base_select$prediccion > 0.096, 1, 0)
 summary(base_select$pcard_hat)
 addmargins(table(base_select$pcard,base_select$pcard_hat))
 prop.table(table(base_select$pcard,base_select$pcard_hat),1)
 prop.table(table(base_select$pcard,base_select$pcard_hat),2)
-addmargins(prop.table(table(base_select$pcard,base_select$pcard_hat)))
 
 ## El MPL se dice que es "intrínsecamente heteroscedástico"
 data_het <- data.frame(pred=Modelo1$fitted.values, res2=Modelo1$residuals^2)
@@ -214,5 +253,3 @@ ggplot(data=data_het, mapping=aes(x=pred, y=res2)) +
 
 #tiramos el modelo robusto
 coeftest(Modelo1, vcov. = vcovHC)
-
-
